47d8240319883bf2bc2c837f309449eb5962d164
Jay Dobies <jason.dobies@redhat.com>
2017-03-07 15:04:54 -0500

    Updated version to match the fact that 1.0 has been released

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='innervate',
-        version='0.1.0',
+        version='1.0.1',
         description='User and load simulator for OpenShift',
         license='GPLv2',
 
